4999f48d09393a1a4660357043c5be903209e45f
NicoleCarvalh <nicolecarvalho6@gmail.com>
2026-08-18 11:36:27 -0300

    atualização model

--- src/main/java/com/senaiflix/model/Series.java
+++ src/main/java/com/senaiflix/model/Series.java
@@ -14,4 +14,32 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 
 public class Series {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 200)
+    private String titulo;
+
+    @Column(nullable = false)
+    private String criador;
+
+    @Column(nullable = false)
+    private Integer anoLancamento;
+
+    @Column(nullable = false)
+    private String genero;
+
+    @Column(nullable = false)
+    private Integer temporadas;
+
+    @Column(nullable = false)
+    private Integer episodios;
+
+    @Column(nullable = false)
+    private BigDecimal notaAvaliacao;
+
+    @Column(nullable = false)
+    private String plataforma;
+
 }
